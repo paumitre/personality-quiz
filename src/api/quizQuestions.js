6b29cc06bbb356c9d95b13602a5e4cd32dc56981
paumitre <73593644+paumitre@users.noreply.github.com>
2024-04-15 15:39:42 -0400

    quiz questions updated

--- src/api/quizQuestions.js
+++ src/api/quizQuestions.js
@@ -1,6 +1,6 @@
 var quizQuestions = [
     {
-        question: "You fall into a puddle and a portal opens. A strange duck named Quill appears holding a gem and a banana.\n Which do you take?",
+        question: "You fall into a puddle and a portal opens. A strange duck named Quill appears holding a gem and a banana. Which do you take?",
         answers: [
             {
                 type: "normie",
@@ -13,15 +13,15 @@ var quizQuestions = [
             {
                 type: "procrastinator",
                 content: "I take whichever one is closer and figure out what’s going on later"
- },
+            },
             {
                 type: "unbothered",
                 content: "Why do I have to take something?"
- },
+            },
             {
                 type: "imposter syndrome",
                 content: "Why am I here? There are much better people for deciding this."
- },
+            },
             {
                 type: "perfectionist",
                 content: "The gem so I have something of value on me"
@@ -30,74 +30,283 @@ var quizQuestions = [
         ]
     },
     {
-        question: "Which console would you prefer to play with friends?",
+
+        question: "Good choice. you follow Quill into the portal and see someone you had a falling out with. Do you…",
         answers: [
             {
-                type: "Microsoft",
-                content: "X-Box"
+                type: "normie",
+                content: "Wave and move on"
             },
             {
-                type: "Nintendo",
-                content: "Nintendo 64"
+                type: "people pleaser",
+                content: "Stop to chat and ask them how they’re doing"
             },
             {
-                type: "Sony",
-                content: "Playstation 1"
+                type: "procrastinator",
+                content: "We never resolved our conflict, so I’m gonna keep walking"
+            },
+            {
+                type: "unbothered",
+                content: "It doesn’t really matter what I do, they already don’t like me"
+            },
+            {
+                type: "imposter syndrome",
+                content: "Keep walking because I know they don’t like me"
+            },
+            {
+                type: "perfectionist",
+                content: "Wave and flash a big smile so I seem like the bigger person"
+
             }
         ]
     },
     {
-        question: "Which of these racing franchises would you prefer to play a game from?",
+
+        question: "Quill leads you to a cottage in an enchanted forest because he needs your help. You remember you have a final assignment due at midnight. What are you thinking?",
         answers: [
             {
-                type: "Microsoft",
-                content: "Forza"
+                type: "normie",
+                content: "I need to take some time to work on my assignment"
             },
             {
-                type: "Nintendo",
-                content: "Mario Kart"
+                type: "people pleaser",
+                content: "I don’t want to bother Quill, hopefully, I can make time later"
             },
             {
-                type: "Sony",
-                content: "Gran Turismo"
+                type: "procrastinator",
+                content: "I’ll do the assignment later"
+            },
+            {
+                type: "unbothered",
+                content: "Eh, I can turn the assignment in late"
+            },
+            {
+                type: "imposter syndrome",
+                content: "I wasn’t sure if I could do the assignment anyway"
+            },
+            {
+                type: "perfectionist",
+                content: "I’m leaving and I will be doing the assignment"
+
             }
         ]
     },
     {
-        question: "Which of these games do you think is best?",
+
+        question: "Your assignment is almost done. Quill tells you he’s throwing a party, but there are only 45 minutes to decorate and bake a cake. He wants to bake a cake, but he leaves the room to take a call. You..",
         answers: [
             {
-                type: "Microsoft",
-                content: "BioShock"
+                type: "normie",
+                content: "Help Quill with what you can before the party"
             },
             {
-                type: "Nintendo",
-                content: "The Legend of Zelda: Ocarina of Time"
+                type: "people pleaser",
+                content: "Start looking for cake ingredients and get started as fast as I can and put up as many decorations as possible"
             },
             {
-                type: "Sony",
-                content: "Final Fantasy VII"
+                type: "procrastinator",
+                content: "Wait for Quill to get back; I'm chilling on the couch"
+            },
+            {
+                type: "unbothered",
+                content: "Why doesn’t he just push the party back?"
+            },
+            {
+                type: "imposter syndrome",
+                content: "Oh my god I’m so bad at baking what’s going to happen?"
+            },
+            {
+                type: "perfectionist",
+                content: "Reread the work on your assignment and help quill after"
+
             }
         ]
     },
     {
-        question: "What console would you prefer to own?",
+        question: "Quill comes back sobbing and wordlessly begins baking. What are you thinking?",
         answers: [
             {
-                type: "Microsoft",
-                content: "X-Box One"
+                type: "normie",
+                content: "Woah, what happened?"
             },
             {
-                type: "Nintendo",
-                content: "Wii U"
+                type: "people pleaser",
+                content: "I need to fix this for Quill–did I do something wrong?"
             },
             {
-                type: "Sony",
-                content: "Playstation 4"
+                type: "procrastinator",
+                content: "I'll let him deal with his emotions and circle back later"
+            },
+            {
+                type: "unbothered",
+                content: "Why is this my problem?"
+            },
+            {
+                type: "imposter syndrome",
+                content: "Oh I don’t think I can help him, i don’t know him like that"
+            },
+            {
+                type: "perfectionist",
+                content: "Oh no…his tears are getting in the batter; I take the spatula from him and take over"
+
             }
         ]
-    }
-  ];
-  
-  export default quizQuestions;
+    },
+    {
+
+        question: "Quill tells you he was on the phone with his manager, Snoffie, and Snoffie wants to kill you…but don’t worry! Quill is going to help you escape. What are you thinking?",
+        answers: [
+            {
+                type: "normie",
+                content: "Okay, let’s get out of here"
+            },
+            {
+                type: "people pleaser",
+                content: "Oh no, what if Quill gets in trouble for helping me escape?"
+            },
+            {
+                type: "procrastinator",
+                content: "I’ll let Quill handle the escape plan"
+            },
+            {
+                type: "unbothered",
+                content: "I bet Snoffie is all talk; I doubt he’ll do anything"
+            },
+            {
+                type: "imposter syndrome",
+                content: "Man I don’t even know how to fight—I’m gonna die"
+            },
+            {
+                type: "perfectionist",
+                content: "I need to do some research on fight techniques"
+
+            }
+        ]
+    },
+    {
+
+        question: "Knock, knock! It’s Snoffie, a big, purple 7ft tall monster. You lock eyes with Snoffie. What are you thinking?",
+        answers: [
+            {
+                type: "normie",
+                content: "Oh my god, I need to leave"
+            },
+            {
+                type: "people pleaser",
+                content: "Why is Snoffie mad at me? I feel we should talk about it, maybe something is going on in his life"
+            },
+            {
+                type: "procrastinator",
+                content: "Maybe if I don’t do anything, then Quill will take care of it for me"
+            },
+            {
+                type: "unbothered",
+                content: "Let’s just see what happens"
+            },
+            {
+                type: "imposter syndrome",
+                content: "I knew I was no match for Snoffie, he’s so much bigger than me"
+            },
+            {
+                type: "perfectionist",
+                content: "I could definitely take Snoffie"
+
+            }
+        ]
+    },
+    {
+        question: "You and Quill join forces to defeat Snoffie. Quill is overjoyed and asks to elope right now. You think to yourself…",
+        answers: [
+            {
+                type: "normie",
+                content: "No thank you!"
+            },
+            {
+                type: "people pleaser",
+                content: "Ugh how do I say no? I don’t want to disappoint him"
+            },
+            {
+                type: "procrastinator",
+                content: "Maybe I can change the subject"
+            },
+            {
+                type: "unbothered",
+                content: "I’m gonna pretend I didn’t hear that"
+            },
+            {
+                type: "imposter syndrome",
+                content: "Why does he want to marry me? Quill is so handsome!"
+            },
+            {
+                type: "perfectionist",
+                content: "Why would I elope? That’s so last minute; I would never elope because I need to plan it"
+
+            }
+        ]
+    },
+    {
+
+        question: "Although Quill loves you, he changes his mind on marriage because he wants you to return home safely. Quill asks if you can do one last favor before you leave. You say…",
+        answers: [
+            {
+                type: "normie",
+                content: "I need to get home, but if it’s a quick favor, sure"
+            },
+            {
+                type: "people pleaser",
+                content: "Of course! How can I help you?"
+            },
+            {
+                type: "procrastinator",
+                content: "I’ll get back to you"
+            },
+            {
+                type: "unbothered",
+                content: "That’s okay, bye"
+            },
+            {
+                type: "imposter syndrome",
+                content: "I was so bad at the fight, why would you want me to help you?"
+            },
+            {
+                type: "perfectionist",
+                content: "Quill, I think you need to do this by yourself"
+
+            }
+        ]
+    },
+    {
+
+        question: "Quill realizes he doesn’t need you for the favor. Eventually, you find your way back to the portal entrance. You have one foot in and one foot out. You see Snoffie in the distance with a scary look on his face, what do you do?",
+        answers: [
+            {
+                type: "normie",
+                content: "Girl, I’m going home. Quill can come if he wants"
+            },
+            {
+                type: "people pleaser",
+                content: "I can fix Snoffie"
+            },
+            {
+                type: "procrastinator",
+                content: "I don’t know???"
+            },
+            {
+                type: "unbothered",
+                content: "I’m going to bed, I’ve had ENOUGH!"
+            },
+            {
+                type: "imposter syndrome",
+                content: "He’s here because I fought him badly so he wants a rematch"
+            },
+            {
+                type: "perfectionist",
+                content: "How do I get rid of Snoffie for good ?"
+
+            }
+        ]
+    },
+];
+
+export default quizQuestions;
 
